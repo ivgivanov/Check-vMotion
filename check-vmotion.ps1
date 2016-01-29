b22c44737365fa6726950b894f19155426f65aa7
ivgivanov <ivaylo@ivgivanov.info>
2016-01-30 00:29:18 +0100

    Net check
    Perform actual vMotion to test network connectivity with a given VM

--- check-vmotion.ps1
+++ check-vmotion.ps1
@@ -6,9 +6,26 @@ Connect-VIServer -server $vCenter
 
 $CSVObjects = @()
 $Results = @()
+$err = @()
 $destpath = [Environment]::GetFolderPath("Desktop")
 $ClusterName = Read-Host -prompt "Enter cluster name"
 $hosts = Get-VMHost -Location $ClusterName | Where {$_.ConnectionState -eq "Connected"} | Sort Name
+
+while($confirmNetTest -ne "y") {
+	$confirmNetTest = Read-Host -prompt "Do you want to perform vMotion network test? This will migrate specified VM across the hosts ensuring the network connectivity (y/n)"
+	If ($confirmNetTest -eq "n") {break}
+	}
+
+If ($confirmNetTest -eq "y") {
+		$TestVMName = Read-Host -prompt "Enter test VM name"
+		while ($TestVM -eq $null) {
+		$TestVM = Get-VM -Name $TestVMName -ErrorAction SilentlyContinue
+			If ($TestVM -eq $null) {$TestVMName = Read-Host "No such VM. Please provide VM name"}
+		}
+}
+
+$IsTestVMAffected = 0
+
 $viewSI = Get-View 'ServiceInstance'
 $viewVmProvChecker = Get-View $viewSI.Content.VmProvisioningChecker
 
@@ -19,7 +36,7 @@ for($i=0; $i -le $hosts.GetUpperBound(0); $i++){
 	$o = $i+1
 	If ($i -eq $hosts.GetUpperBound(0)) { $o = 0}
 
-	Write-Host "Testing vMotion compatibility between" $hosts[$i] "and" $hosts[$o]
+	Write-Host "Testing VM vMotion compatibility between" $hosts[$i] "and" $hosts[$o]
 
 	foreach($vm in $vms){
 
@@ -29,6 +46,7 @@ for($i=0; $i -le $hosts.GetUpperBound(0); $i++){
 				   foreach ($Error in $Record.Error) {
 					$CSVObject = new-object PSObject
 					$CSVObject | add-member -membertype NoteProperty -name "VM" -value $Record.VM
+					If ($Record.VM -eq $TestVM.Id) {$IsTestVMAffected = 1} 
 					$CSVObject | add-member -membertype NoteProperty -name "VMHost" -value $Record.Host
 					$CSVObject | add-member -membertype NoteProperty -name "Error" -value $Error.LocalizedMessage
 					$CSVObjects += $CSVObject
@@ -38,6 +56,22 @@ for($i=0; $i -le $hosts.GetUpperBound(0); $i++){
 
 	}
 
+}
+
+If ($confirmNetTest -eq "y") {
+	If ($IsTestVMAffected -eq 0) {
+	Write-Host "Testing vMotion network connectivity by migrating" $TestVM "across the hosts in" $ClusterName
+	$InitialVMHost = $TestVM.VMHost
+		for ($i=0; $i -le $hosts.GetUpperBound(0); $i++) {
+				If ($i -eq $hosts.GetUpperBound(0)) {
+				$DestHost = $hosts[0]
+				} else { $DestHost = $hosts[$i+1] }
+			
+			Move-VM -VM $TestVM -Destination $DestHost -ErrorVariable +err 2>> $destpath\vMotionNetTest.txt | out-null
+		}	
+		If ($err.count -eq 0) {Write-Output "Network test completed successfully" >> $destpath\vMotionNetTest.txt}
+		If ($InitialVMHost -ne $hosts[0]) { Move-VM -VM $TestVM -Destination $InitialVMHost }
+	} else { Write-Output "The specified test VM cannot be migrated. Please check vMotionTest.csv file for details" >> $destpath\vMotionNetTest.txt }
 }
 
 Disconnect-VIServer -Confirm:$False
